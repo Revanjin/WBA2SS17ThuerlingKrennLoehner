eec41cd3b84d59aeffd94c0ec75a9638f6cfaf1d
= <=>
2017-04-29 17:02:30 +0200

    Aufgabe 2, Chalk hinzugefuegt

--- Thuerling/index.js
+++ Thuerling/index.js
@@ -1,17 +1,19 @@
 var fs = require('fs');
+var chalk = require('chalk');
 
 	// 	Read the File staedte.json , convert text to js obj
 	fs.readFile('staedte.json', function(err, data){
 		var staedte = JSON.parse(data);
 	
-	// 	ForSchleife zum ausgeben der bereits gelesenen daten
-	for(var i=0; i<staedte.cities.length; i++){
-		console.log('Name:       '+staedte.cities[i].name);
-		console.log('Country:    '+staedte.cities[i].country);
-		console.log('Population: '+staedte.cities[i].population);
-	//	Damit nach jeder Ausgabe der Daten auch abgetrennt wird	
-		if(i < staedte.cities.length - 1){
-			console.log("--------------------");
+		// 	ForSchleife zum ausgeben der bereits gelesenen daten
+		//	Fuer Aufgabe2 - Chalk Module hinzugefuegt
+		for(var i=0; i<staedte.cities.length; i++){
+			console.log(chalk.white.bgRed.bold('Name:       '+staedte.cities[i].name));
+			console.log(chalk.gray.bgCyan.bold('Country:    '+staedte.cities[i].country));
+			console.log(chalk.gray.bgWhite.bold('Population: '+staedte.cities[i].population));
+		//	Damit nach jeder Ausgabe der Daten auch abgetrennt wird	
+			if(i < staedte.cities.length - 1){
+				console.log("--------------------");
+			}
 		}
-	}
 	});
