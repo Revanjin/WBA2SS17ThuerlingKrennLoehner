f921129bd3b2b6a8b5bd1f7c8bc17bffd6bec4e6
Cuhater <christian_krenn@hotmail.de>
2017-05-07 17:46:01 +0200

    KickOffAufgabe 2 Chalk

--- Krenn/index.js
+++ Krenn/index.js
@@ -1,13 +1,14 @@
 var fs = require('fs');
+var chalk = require('chalk');
 
 fs.readFile('staedte.json', function(err, data){
 		var staedte = JSON.parse(data);
 		
 
 for(var i=0; i<staedte.cities.length; i++){
-			console.log('Name:       '+staedte.cities[i].name);
-			console.log('Country:    '+staedte.cities[i].country);
-			console.log('Population: '+staedte.cities[i].population);
+			console.log(chalk.red('Name:       '+staedte.cities[i].name));
+			console.log(chalk.white('Country:    '+staedte.cities[i].country));
+			console.log(chalk.cyan('Population: '+staedte.cities[i].population));
 		//	Damit nach jeder Ausgabe der Daten auch abgetrennt wird	
 			if(i < staedte.cities.length - 1){
 				console.log("--------------------");
