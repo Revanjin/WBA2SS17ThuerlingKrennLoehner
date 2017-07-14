e376fa7a64122567870573915542423bfd4904fc
Cuhater <christian_krenn@hotmail.de>
2017-07-14 16:14:06 +0200

    Dienstnutzer basisaufbau

--- FM2_Dienstnutzer/app.js
+++ FM2_Dienstnutzer/app.js
@@ -1,31 +1,30 @@
-var http = require('http');
-var path = require('path');
 var express = require('express');
-var request = require('request');
-var app = express();
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var session = require('express-session');
 
 var index = require('./routes/index');
-var dashboard = require('./routes/dashboard');
+var users = require('./routes/users');
 
-var dHost = 'http://localhost';
-var dPort = 8000;
-var dUrl = dHost + ':' + dPort;
+var app = express();
 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(session({secret:"asdfasdfasdfasdfasdf", resave: false, saveUninitaialized:true}))
 app.use(express.static(path.join(__dirname, 'public')));
 
-// - routen definieren
 app.use('/', index);
-app.use('/dashboard', dashboard);
+app.use('/users', users);
 
-// -- Errorhandling --
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
