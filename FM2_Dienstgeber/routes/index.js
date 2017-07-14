a5068f12b554de9664be828488d4e0f220f7181c
Cuhater <christian_krenn@hotmail.de>
2017-07-14 04:35:58 +0200

    index.js kommentiert

--- FM2_Dienstgeber/routes/index.js
+++ FM2_Dienstgeber/routes/index.js
@@ -1,73 +1,77 @@
+//  require definieren
 var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
+// DB Connection aufbauen
 mongoose.connect('localhost:27017/test');
 var Schema = mongoose.Schema;
 
-
+// User Schema in collection myuser erstellen
 var UserSchema = new mongoose.Schema({
     username: {type: String, unique: true},
     password: {type: String},
-    //firstname: String,
-    //lastname: String
 
 }, {collection: 'myuser'});
-
+// Model in Var speichern
 var User = mongoose.model('myuser', UserSchema);
 
-/* GET home page. */
+
+// -------------- GET / --------------
 router.get('/', function(req, res, next) {
+  // JSON response
+  if (req.header('Content-Type') == 'application/json')
+  {
+    res.write('**********************************************************\n');
+    res.write('*****  Willkommen bei Find Mates for Matches [ FM2 ] *****\n');
+    res.write('*****  Dir stehen folgenden Funktionen zur Verfügung *****\n');
+    res.write('*****  get | post                                    *****\n');
+    res.write('*****  auf folgende Ressourcen                       *****\n');
+    res.write('*****  / . /login . /register                        *****\n');
+    res.write('**********************************************************\n');
+    console.log('Willkommen bei Find Mates for Matches [ FM2 ]\n');
+    res.end();
+  }
+  else
+  {
+  // HTML response
+  // render login.hbs
   res.render('login');
+  }
 });
 
-/*
-router.post('/login', function(req, res){
-  var username = req.body.username;
-  var password = req.body.password;
-
-  User.findOne({username: username, password: password}, function(err, user){
-    if(err){
-      console.log(err);
-      return res.status(500).send();
-    }
-    if(!user){
-      return res.status(404).send();
-    }
-
-      req.session.user = user;
-      res.status(200).send();
-      res.redirect('/dashboard');
-
-
-  })
-
-});
-*/
+// -------------- POST /LOGIN --------------
 router.post('/login', function(req, res)
 {
+// username und password definieren
 var username = req.body.username;
 var password = req.body.password;
+  // JSON response
   if (req.header('Content-Type') == 'application/json') {
+    // Mongoose funktion findOne
     User.findOne({username: username, password: password}, function(err, user)
     {
+      // Wenn User nicht gefunden 404 Not Found
       if(!user)
       {
         return res.status(404).send();
       }
+      // UserSession + response
       req.session.user = user;
       res.send('Willkommen bei FM2 '+ req.body.username +'. du wurdest erfolgreich eingeloggt!');
       console.log('Willkommen bei FM2. Du wurdest erfolgreich eingeloggt!');
-      //return res.json({ users: users });
     })
   }
+  // HTML response
   else
   {
     User.findOne({username: username, password: password}, function(err, user)
     {
+      // Wenn User nicht gefunden 404 Not Found
       if(!user)
       {
         return res.status(404).send();
       }
+        // UserSession + response
         req.session.user = user;
         res.status(200).send();
         res.redirect('/dashboard');
@@ -75,57 +79,17 @@ var password = req.body.password;
   }
 });
 
-
-/*
-router.post('/login:format?', function(req, res, next)
-{
-  var username = req.body.username;
-  var password = req.body.password;
-
-  // req param object
-  if (req.params.format == '.json')
-  {
-    console.log('1111111!:');
-    User.findOne({username: username, password: password}, function(err, user)
-    {
-      if(!user)
-      {
-        return res.status(404).send();
-      }
-      req.session.user = user;
-      res.status(200).res.send('Welcome!');
-      console.log('Welcome!:');
-    })
-  }
-  console.log('222222!:');
-  // else redirect html
-  User.findOne({username: username, password: password}, function(err, user)
-  {
-    if(!user)
-    {
-      return res.status(404).send();
-    }
-      req.session.user = user;
-      res.status(200).send();
-      console.log('Goodbye!:');
-      res.redirect('/dashboard');
-  })
-});
-
-/*router.get('/dashboard'), function(req, res){
-  if(!req.session.user){
-    return res.status(401).send();
-  }
-  return res.status(200).send("Welcome");
-}
-*/
+// -------------- GET /LOGOUT --------------
 router.get('/logout', function(req, res){
+  // JSON response
   if (req.header('Content-Type') == 'application/json')
   {
+    // Überprüft ob User eingeloggt wenn nicht 404 Not Found
     if(!req.session.user)
     {
-      return res.status(401).send('Kein eingeloggter Benutzer gefunden. Bitte melde dich an um dich abzumelden.');
+      return res.status(404).send('Kein eingeloggter Benutzer gefunden. Bitte melde dich an um dich abzumelden.');
     }
+    // Session terminieren und response Infotext
     else
     {
     req.session.destroy();
@@ -133,6 +97,8 @@ router.get('/logout', function(req, res){
     console.log('Bis zum nächsten mal. Du wurdest erfolgreich ausgeloggt!');
     }
   }
+  // HTML response
+  // Session terminieren und redirect auf Loginpage
   else
   {
     req.session.destroy();
@@ -140,43 +106,52 @@ router.get('/logout', function(req, res){
   }
 });
 
-/* Register. */
+// -------------- GET /REGISTER --------------
 router.get('/register', function(req, res, next) {
-
+  // JSON response
   if (req.header('Content-Type') == 'application/json')
   {
+    //response Infotext
     return res.status(200).send('Du kannst dich per post auf die Ressource /register registrieren');
     console.log('Du kannst dich per post auf die Ressource /register registrieren');
   }
   else
   {
+    // render register.hbs
     res.render('register');
   }
 });
 
+// -------------- POST /REGISTER --------------
 router.post('/register', function(req, res, next) {
 
+  // username und password definieren
   var username = req.body.username;
   var password = req.body.password;
+  // newuser Objekt erstellen
   var newuser = new User();
-
+  // werte in newuser. speichern
   newuser.username = username;
   newuser.password = password;
-
+  // JSON response
   if (req.header('Content-Type') == 'application/json')
   {
+    // mongoose funktion save
     newuser.save(function(err, savedUser){
       if(err)
       {
         console.log(err);
         return res.status(500).send();
       }
+      // response Infotext
       res.send('Willkommen bei FM2. Der Benutzer mit dem Namen '+ newuser.username +'. wurde erfolgreich angelegt!');
       console.log('Willkommen bei FM2. Der Benutzer wurde erfolgreich angelegt!');
     })
   }
+  // HTML response
   else
   {
+    // mongoose funktion save
     newuser.save(function(err, savedUser){
       if(err)
       {
@@ -188,5 +163,5 @@ router.post('/register', function(req, res, next) {
 
   }
 });
-
+// exportiere router
 module.exports = router;
